717a019f407c8c5ea2fbe5055086850b5fe13171
Pavel <pavel.gavriluk@collibra.com>
2019-06-18 13:10:45 -0400

    Adding unit tests and integration tests.

---
+++ src/test/java/demo/pavel/recipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,46 @@
+package demo.pavel.recipe.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import demo.pavel.recipe.model.Recipe;
+import demo.pavel.recipe.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+	RecipeServiceImpl recipeService;
+
+	@Mock
+	RecipeRepository recipeRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+
+		recipeService = new RecipeServiceImpl(recipeRepository);
+	}
+
+	@Test
+	public void findRecipes() {
+		Recipe recipe = new Recipe();
+		HashSet recipesData = new HashSet();
+		recipesData.add(recipe);
+
+		when(recipeRepository.findAll()).thenReturn(recipesData);
+
+		Set<Recipe> recipes = recipeService.findRecipes();
+
+		assertEquals(1, recipes.size());
+		verify(recipeRepository, times(1)).findAll();
+	}
+}

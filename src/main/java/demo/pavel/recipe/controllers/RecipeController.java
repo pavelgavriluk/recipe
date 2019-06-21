6bdf1e26093602f4d385ca5658be7be0253c2586
Pavel <pavel.gavriluk@collibra.com>
2019-06-21 15:49:39 -0400

    Adding mapping to controller.

--- src/main/java/demo/pavel/recipe/controllers/RecipeController.java
+++ src/main/java/demo/pavel/recipe/controllers/RecipeController.java
@@ -2,9 +2,12 @@ package demo.pavel.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import demo.pavel.recipe.commands.RecipeCommand;
 import demo.pavel.recipe.services.RecipeService;
 
 @Controller
@@ -22,5 +25,19 @@ public class RecipeController {
 		model.addAttribute("recipe", recipeService.findRecipeById(Long.valueOf(id)));
 
 		return "recipe/show";
+	}
+
+	@RequestMapping("recipe/new")
+	public String newRecipe(Model model){
+		model.addAttribute("recipe", new RecipeCommand());
+
+		return "recipe/recipeform";
+	}
+
+	@PostMapping("recipe")
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+		return "redirect:/recipe/show/" + savedCommand.getId();
 	}
 }
